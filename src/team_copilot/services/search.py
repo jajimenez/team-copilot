72029231752c0646130734aaf72c753fcc0576be
Jose A. Jimenez <jajimenezcarm@gmail.com>
2025-04-28 01:55:49 +0000

    Reformat comments

--- src/team_copilot/services/search.py
+++ src/team_copilot/services/search.py
@@ -42,19 +42,20 @@ def get_most_similar_chunks(query: str, limit: int = 5) -> list[DocumentChunk]:
     # Search for the closest chunks in the database
     try:
         with open_session(settings.db_url) as session:
-            # Create the statement. This is a raw SQL statement because the "<=>" operator
-            # is specific to the "pgvector" extension of PostgreSQL and isn't supported by
-            # SQLModel.
+            # Create the statement. This is a raw SQL statement because the "<=>"
+            # operator is specific to the "pgvector" extension of PostgreSQL and isn't
+            # supported by SQLModel.
 
-            # The "<=>" operator returns the cosine distance between two vectors,
-            # which is a number between 0 (vectors are identical) and 2 (vectors point in
-            # opposite directions). A value of 1 means that the vectors are perpendicular.
-            # Therefore, the smaller the value, the more similar the vectors are.
+            # The "<=>" operator returns the cosine distance between two vectors, which
+            # is a number between 0 (vectors are identical) and 2 (vectors point in
+            # opposite directions). A value of 1 means that the vectors are
+            # perpendicular. Therefore, the smaller the value, the more similar the
+            # vectors are.
 
-            # We format the statement string with the "emb" variable between single quotes,
-            # which is the format of the "embedding" vector column in SQL statements. We
-            # can't use the "params" argument of the "text" function because it doesn't
-            # translate "emb" to the proper format.
+            # We format the statement string with the "emb" variable between single
+            # quotes, which is the format of the "embedding" vector column in SQL
+            # statements. We can't use the "params" argument of the "text" function
+            # because it doesn't translate "emb" to the proper format.
 
             s = text(
                 "SELECT dc.id "
