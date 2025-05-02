645fdd8b7aa2fd3514a002f0487fc995ccd1a9b2
Jose A. Jimenez <jajimenezcarm@gmail.com>
2025-05-02 18:20:13 +0000

    Implement function to update a user

--- src/team_copilot/routers/users.py
+++ src/team_copilot/routers/users.py
@@ -1,13 +1,16 @@
 """Team Copilot - Routers - Health."""
 
+from uuid import UUID
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Form, status
+from fastapi import APIRouter, Depends, Form, Path, status
 from fastapi.exceptions import RequestValidationError, HTTPException
+
+
 
 from team_copilot.core.auth import get_enabled_user, get_admin_user
 from team_copilot.models.data import User
-from team_copilot.models.request import CreateUserRequest
+from team_copilot.models.request import CreateUserRequest, UpdateUserRequest
 
 from team_copilot.models.response import (
     MessageResponse,
@@ -25,11 +28,16 @@ CRE_USER_SUM = "Create a user"
 CUR_USER = "Current user"
 GET_CUR_USER_DESC = "Get the current authenticated user."
 GET_CUR_USER_SUM = "Get the current authenticated user"
+UPD_USER_DESC = "Update a user. Only administrator users are authorized."
+UPD_USER_SUM = "Update a user"
 USER_CRE_1 = "User created."
 USER_CRE_2 = "User {} ({}) created."
-USER_EXISTS = "A user with the same username or e-mail address exists."
-USER_UPD = "User updated."
 USER_DATA = "User data."
+USER_EXISTS = "A user with the same username or e-mail address exists."
+USER_ID = "User ID."
+USER_NF_1 = "User not found."
+USER_NF_2 = "User {} not found."
+USER_UPD = "User updated."
 
 # Router
 router = APIRouter(
@@ -53,8 +61,13 @@ router = APIRouter(
     operation_id="get_current_user",
     summary=GET_CUR_USER_SUM,
     description=GET_CUR_USER_DESC,
-    responses={status.HTTP_200_OK: {"description": CUR_USER}},
-    response_model=UserResponse,
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {
+            "description": CUR_USER,
+            "model": UserResponse,
+        }
+    },
 )
 def get_current_user(user: Annotated[User, Depends(get_enabled_user)]) -> UserResponse:
     """Get the current authenticated user.
@@ -112,6 +125,77 @@ async def create_user(
 
     # Save the User object to the database. The ID of the user is set by the database
     # and is set in the User object by "save_doc".
+    save_user(u)
+
+    # Return the user ID and a message
+    return UserSavedResponse(user_id=u.id, message=USER_CRE_2.format(u.id, u.username))
+
+
+@router.put(
+    "/{id}",
+    operation_id="update_user",
+    summary=UPD_USER_SUM,
+    description=UPD_USER_DESC,
+    dependencies=[Depends(get_admin_user)],
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {
+            "description": USER_UPD,
+            "model": UserSavedResponse,
+        },
+        status.HTTP_404_NOT_FOUND: {
+            "description": USER_NF_1,
+            "model": MessageResponse,
+        },
+        status.HTTP_409_CONFLICT: {
+            "description": USER_EXISTS,
+            "model": MessageResponse,
+        },
+    },
+)
+async def update_user(
+    id: Annotated[UUID, Path(description=USER_ID)],
+    user: Annotated[UpdateUserRequest, Form(description=USER_DATA)],
+) -> UserSavedResponse:
+    """Update a user.
+
+    Args:
+        id (UUID): User ID.
+        user (CreateUserRequest): User data.
+
+    Raises:
+        RequestValidationError: If the any field is invalid.
+        HTTPException: If another user with the same username or e-mail address exists.
+
+    Returns:
+        UserSavedResponse: ID of the user and a message.
+    """
+    # Check that the user exists and get it
+    u = get_user(id=id)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=USER_NF_2.format(id),
+        )
+
+    # Check that another user with the same username or e-mail address doesn't exist
+    other_user = get_user(username=user.username, email=user.email)
+
+    if other_user and other_user.id != id:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=USER_EXISTS)
+
+    # Update the User object. We set the "updated_at" field to None to let the database
+    # set it to the current timestamp when we save it to the database.
+    u.username = user.username
+    u.name = user.name
+    u.email = user.email
+    u.staff = user.staff
+    u.admin = user.admin
+    u.enabled = user.enabled
+    u.updated_at = None
+
+    # Save the User object to the database
     save_user(u)
 
     # Return the user ID and a message
