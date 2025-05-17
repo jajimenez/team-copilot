6f1d00d61ea3c5b50eab4e7e589e9d945181e79f
Jose A. Jimenez <jajimenezcarm@gmail.com>
2025-05-17 19:40:59 +0000

    Refactor tests

---
+++ tests/unit/routers/users/test_get_all_users.py
@@ -0,0 +1,110 @@
+"""Team Copilot Tests - Unit Tests - Routers - Users - Get All Users."""
+
+from unittest.mock import patch
+
+from dateutil.parser import parse
+
+from fastapi import FastAPI, status
+from fastapi.testclient import TestClient
+
+from team_copilot.core.auth import get_admin_user
+from team_copilot.models.data import User
+
+from tests.unit.routers import raise_not_authorized_exc
+
+
+def test_get_all_users(
+    app: FastAPI,
+    test_client: TestClient,
+    admin_user_mock: User,
+    users_mock: list[User],
+):
+    """Test the "get_all_users" endpoint.
+
+    Args:
+        app (FastAPI): FastAPI application.
+        test_client (TestClient): FastAPI test client.
+        admin_user_mock (User): Enabled administrator user mock.
+        users_mock (list[User]): Users mock.
+    """
+    app.dependency_overrides[get_admin_user] = lambda: admin_user_mock
+
+    with patch("team_copilot.routers.users.get_all_us", return_value=users_mock):
+        # Make HTTP request
+        response = test_client.get("/users")
+        assert response.status_code == status.HTTP_200_OK
+
+        user_count = len(users_mock)
+
+        res_data = response.json()
+        assert len(res_data) == 3
+        assert res_data["message"] == f"{user_count} users retrieved."
+        assert res_data["count"] == user_count
+
+        data = res_data["data"]
+        assert len(data) == user_count
+
+        for i, u in enumerate(data):
+            assert u["id"] == str(users_mock[i].id)
+            assert u["username"] == users_mock[i].username
+            assert u["name"] == users_mock[i].name
+            assert u["email"] == users_mock[i].email
+            assert u["staff"] == users_mock[i].staff
+            assert u["admin"] == users_mock[i].admin
+            assert u["enabled"] == users_mock[i].enabled
+            assert parse(u["created_at"]) == users_mock[i].created_at
+            assert parse(u["updated_at"]) == users_mock[i].updated_at
+
+    app.dependency_overrides.clear()
+
+
+def test_get_all_users_unauthenticated(test_client: TestClient):
+    """Test the "get_all_users" endpoint for an unauthenticated user.
+
+    Args:
+        test_client (TestClient): FastAPI test client.
+    """
+    # Make HTTP request
+    response = test_client.get("/users")
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    res_data = response.json()
+    assert len(res_data) == 3
+
+    assert res_data["message"] == "Authentication error."
+    assert res_data["count"] == 1
+
+    data = res_data["data"]
+    assert len(data) == 1
+
+    assert data[0]["id"] == "authentication"
+    assert data[0]["message"] == "Not authenticated"
+
+
+def test_get_all_users_unauthorized(app: FastAPI, test_client: TestClient):
+    """Test the "get_all_users" endpoint for an unauthorized (disabled or not
+    administrator) user.
+
+    Args:
+        app (FastAPI): FastAPI application.
+        test_client (TestClient): FastAPI test client.
+    """
+    app.dependency_overrides[get_admin_user] = raise_not_authorized_exc
+
+    # Make HTTP request
+    response = test_client.get("/users")
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    res_data = response.json()
+    assert len(res_data) == 3
+
+    assert res_data["message"] == "Authorization error."
+    assert res_data["count"] == 1
+
+    data = res_data["data"]
+    assert len(data) == 1
+
+    assert data[0]["id"] == "authorization"
+    assert data[0]["message"] == "Not authorized"
+
+    app.dependency_overrides.clear()
---
+++ tests/unit/routers/users/test_get_current_user.py
@@ -0,0 +1,106 @@
+"""Team Copilot Tests - Unit Tests - Routers - Users - Get Current User."""
+
+from unittest.mock import patch
+
+from dateutil.parser import parse
+
+from fastapi import FastAPI, status
+from fastapi.testclient import TestClient
+
+from team_copilot.core.auth import get_enabled_user
+from team_copilot.models.data import User
+
+from tests.unit.routers import raise_not_authorized_exc
+
+
+def test_get_current_user(
+    app: FastAPI,
+    test_client: TestClient,
+    enabled_user_mock: User,
+):
+    """Test the "get_current_user" endpoint.
+
+    Args:
+        app (FastAPI): FastAPI application.
+        test_client (TestClient): FastAPI test client.
+        enabled_user_mock (User): Enabled user mock.
+    """
+    app.dependency_overrides[get_enabled_user] = lambda: enabled_user_mock
+
+    with patch("team_copilot.routers.users.get_us", return_value=enabled_user_mock):
+        # Make HTTP request
+        response = test_client.get("/users/me")
+        assert response.status_code == status.HTTP_200_OK
+
+        res_data = response.json()
+        assert len(res_data) == 2
+
+        assert res_data["message"] == (
+            f"User {enabled_user_mock.id} ({enabled_user_mock.username}) retrieved."
+        )
+
+        data = res_data["data"]
+
+        assert data["id"] == str(enabled_user_mock.id)
+        assert data["username"] == enabled_user_mock.username
+        assert data["name"] == enabled_user_mock.name
+        assert data["email"] == enabled_user_mock.email
+        assert data["staff"] == enabled_user_mock.staff
+        assert data["admin"] == enabled_user_mock.admin
+        assert data["enabled"] == enabled_user_mock.enabled
+        assert parse(res_data["data"]["created_at"]) == enabled_user_mock.created_at
+        assert parse(res_data["data"]["updated_at"]) == enabled_user_mock.updated_at
+
+    app.dependency_overrides.clear()
+
+
+def test_get_current_user_unauthenticated(test_client: TestClient):
+    """Test the "get_current_user" endpoint for an unauthenticated user.
+
+    Args:
+        app (FastAPI): FastAPI application.
+        test_client (TestClient): FastAPI test client.
+    """
+    # Make HTTP request
+    response = test_client.get("/users/me")
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    res_data = response.json()
+    assert len(res_data) == 3
+
+    assert res_data["message"] == "Authentication error."
+    assert res_data["count"] == 1
+
+    data = res_data["data"]
+    assert len(data) == 1
+
+    assert data[0]["id"] == "authentication"
+    assert data[0]["message"] == "Not authenticated"
+
+
+def test_get_current_user_unauthorized(app: FastAPI, test_client: TestClient):
+    """Test the "get_current_user" endpoint for an unauthorized (disabled) user.
+
+    Args:
+        app (FastAPI): FastAPI application.
+        test_client (TestClient): FastAPI test client.
+    """
+    app.dependency_overrides[get_enabled_user] = raise_not_authorized_exc
+
+    # Make HTTP request
+    response = test_client.get("/users/me")
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    res_data = response.json()
+    assert len(res_data) == 3
+
+    assert res_data["message"] == "Authorization error."
+    assert res_data["count"] == 1
+
+    data = res_data["data"]
+    assert len(data) == 1
+
+    assert data[0]["id"] == "authorization"
+    assert data[0]["message"] == "Not authorized"
+
+    app.dependency_overrides.clear()
